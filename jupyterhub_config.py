9dfce9c83db1198b067ae0d088480c3a105f2820
kxxt <rsworktech@outlook.com>
2022-01-12 17:53:48 +0800

    Setting IP Address.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,12 +1,17 @@
 # Configuration file for jupyterhub.
 import os
 
+c = get_config()
+
 c.JupyterHub.active_server_limit = 4
 c.JupyterHub.cleanup_servers = True
 c.JupyterHub.concurrent_spawn_limit = 3
 c.JupyterHub.cookie_max_age_days = 7
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 
+from jupyter_client.localinterfaces import public_ips
+c.JupyterHub.hub_ip = public_ips()[0]
+print(c.JupyterHub.hub_ip)
 
 notebook_dir = os.environ.get(
     'DOCKER_NOTEBOOK_DIR') or '/home/jovyan/persistent'
