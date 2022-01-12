3b312ca932501b90d98ec74c1aec89391eff8bf3
kxxt <rsworktech@outlook.com>
2022-01-12 23:50:08 +0800

    chore: Clean up.

--- backend/user.py
+++ backend/user.py
@@ -82,11 +82,6 @@ async def register(form_data: RegisterInfo, db: Session = Depends(get_db)):
     return User(username=user.username)
 
 
-@router.get("/users/me")
+@router.get("/user-data")
 async def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
-
-
-@router.get("/users/me/items/")
-async def read_own_items(current_user: User = Depends(get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
