176707f3f7c7b2a92799c0ff14675b0650bd0dc5
kxxt <rsworktech@outlook.com>
2022-01-13 18:33:21 +0800

    Fix root_dir in nginx conf template.

--- generate_nginx_conf.py
+++ generate_nginx_conf.py
@@ -1,4 +1,7 @@
 from backend.config import port
+import pathlib
+
+root = pathlib.Path(__file__).parent.resolve()
 
 DOMAIN_NAME = "hub.kxxt.tech"
 
@@ -9,6 +12,8 @@ with open(f"{DOMAIN_NAME}.conf", "w", encoding='utf-8') as f:
     f.write(template
             .replace("{api_port}", str(port))
             .replace("{domain_name}", DOMAIN_NAME)
-            .replace("{hub_port}", str(8000)))
+            .replace("{hub_port}", str(8000))
+            .replace("{root_dir}", str(root))
+            )
 
 print("Success!")
