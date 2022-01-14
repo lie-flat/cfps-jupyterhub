17f0c44803b030d3d74e27344644907bcb098f6e
kxxt <rsworktech@outlook.com>
2022-01-14 19:48:19 +0800

    Fix url path mismatch in jupyterhub_config.py.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -2,11 +2,10 @@
 import os
 import sys
 import pathlib
-from jupyter_client.localinterfaces import public_ips
 
 sys.path.append(str(pathlib.Path(__file__).parent.resolve()))
 
-from backend.config import HUB_CLIENT_ID, HUB_CLIENT_SECRET
+from backend.config import HUB_CLIENT_ID, HUB_CLIENT_SECRET, port
 
 c = get_config()
 
@@ -16,10 +15,11 @@ c.JupyterHub.concurrent_spawn_limit = 3
 c.JupyterHub.cookie_max_age_days = 7
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 
-c.JupyterHub.bind_url = 'http://127.0.0.1:8000'  # 因为有 nginx, 所以代理只监听 127.0.0.1:8000 即可
-c.JupyterHub.base_url = '/jupyter'
+c.JupyterHub.bind_url = 'http://127.0.0.1:8000/jupyter'  # 因为有 nginx, 所以代理只监听 127.0.0.1:8000 即可
 
-c.JupyterHub.hub_ip = public_ips()[0]
+c.JupyterHub.hub_ip = '172.17.0.1'  # 设置为 Docker 虚拟网卡的 IP
+c.DockerSpawner.hub_ip_connect = '172.17.0.1'
+c.DockerSpawner.hub_connect_url = "http://172.17.0.1:8081/"
 print(c.JupyterHub.hub_ip)
 
 notebook_dir = os.environ.get(
@@ -39,7 +39,7 @@ c.GenericOAuthenticator.oauth_callback_url = '/jupyter/oauth_callback'
 c.GenericOAuthenticator.client_id = HUB_CLIENT_ID
 c.GenericOAuthenticator.client_secret = HUB_CLIENT_SECRET
 c.GenericOAuthenticator.login_service = '统一身份认证服务'
-c.GenericOAuthenticator.userdata_url = '/api/user-data'
-c.GenericOAuthenticator.token_url = '/api/user-login'
+c.GenericOAuthenticator.userdata_url = f'http://127.0.0.1:{port}/user-data'
+c.GenericOAuthenticator.token_url = f'http://127.0.0.1:{port}/token'
 c.GenericOAuthenticator.authorize_url = '/login'
 c.GenericOAuthenticator.username_key = 'username'
