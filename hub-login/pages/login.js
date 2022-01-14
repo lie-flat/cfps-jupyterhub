604eec24ff456b9030ab3c98fdae8e705015ec47
kxxt <rsworktech@outlook.com>
2022-01-14 19:47:20 +0800

    Implement Auth (frontend).

--- hub-login/pages/login.js
+++ hub-login/pages/login.js
@@ -18,39 +18,46 @@ const getParams = () => {
 }
 
 export default function Login() {
-    const form = (<Formik initialValues={{username: '', password: ''}}
-
-                          onSubmit={(values, helpers) => {
-                              let formData = new FormData();
-                              formData.append('username', values.username);
-                              formData.append('password', values.password);
-                              let params = getParams();
-                              let r = axios.post('/api/user-login', formData)
-                              r.then(res => console.log(res)).catch(err => {
-                                  if (err?.response?.data?.detail) {
-                                      helpers.setFieldError('username', err.response.data.detail);
-                                  } else {
-                                      helpers.setFieldError('username', '未知错误');
-                                  }
-                              });
-                              helpers.setSubmitting(false);
-                          }}>
-        {({isSubmitting}) => (<Form>
-            <FieldContainer>
-                <UserIcon/>
-                <Field type="text" name="username" placeholder="请输入用户名" required/>
-            </FieldContainer>
-            <StyledErrorMessage name="username" component="div"/>
-            <FieldContainer>
-                <PasswordIcon/>
-                <Field type="password" name="password" placeholder="请输入密码"/>
-            </FieldContainer>
-            <StyledErrorMessage name="password" component="div"/>
-            <StyledButton type="submit" disabled={isSubmitting}>
-                登录
-            </StyledButton>
-        </Form>)}
-    </Formik>);
+    const form = (
+        <Formik initialValues={{username: '', password: ''}}
+                onSubmit={(values, helpers) => {
+                    let formData = new FormData();
+                    formData.append('username', values.username);
+                    formData.append('password', values.password);
+                    let params = getParams();
+                    let r = axios.post('/api/user-login', formData)
+                    r.then(res => {
+                        let redirect_uri = params.get('redirect_uri');
+                        if (redirect_uri) {
+                            redirect_uri += '?code=' + res.data.access_token + '&state=' + params.get('state');
+                            window.location.href = redirect_uri;
+                        }
+                    }).catch(err => {
+                        if (err?.response?.data?.detail) {
+                            helpers.setFieldError('username', err.response.data.detail);
+                        } else {
+                            helpers.setFieldError('username', '我们这边出现了一些问题，请联系网站管理员');
+                        }
+                    });
+                    helpers.setSubmitting(false);
+                }}>
+            {({isSubmitting}) => (<Form>
+                <FieldContainer>
+                    <UserIcon/>
+                    <Field type="text" name="username" placeholder="请输入用户名" required/>
+                </FieldContainer>
+                <StyledErrorMessage name="username" component="div"/>
+                <FieldContainer>
+                    <PasswordIcon/>
+                    <Field type="password" name="password" placeholder="请输入密码"/>
+                </FieldContainer>
+                <StyledErrorMessage name="password" component="div"/>
+                <StyledButton type="submit" disabled={isSubmitting}>
+                    登录
+                </StyledButton>
+            </Form>)}
+        </Formik>
+    );
 
     return (
         <Container>
