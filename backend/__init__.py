97014abbf7a01055855f6d0814df920e98afdf90
kxxt <rsworktech@outlook.com>
2022-01-15 09:15:46 +0800

    Rate limit: 60/min (backend)

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+
 import backend.user
 from .db import engine, BaseDBModel
+from .limiter import limiter
 
 BaseDBModel.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 
 @app.get("/")
