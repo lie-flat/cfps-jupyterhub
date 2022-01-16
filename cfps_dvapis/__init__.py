6041047f407a982533dd02088c19454524e60aa2
kxxt <rsworktech@outlook.com>
2022-01-16 14:38:04 +0800

    Initialize cfps_dvapis.

---
+++ cfps_dvapis/__init__.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+
+from os import environ
+
+DATABASE_PORT = environ.get('DATABASE_PORT', '3306')
+DATABASE_USER = environ.get('DATABASE_USER', 'jupyterhub')
+DATABASE_PASSWORD = environ.get('DATABASE_PASSWORD', 'SUPER-secret_PlavsW0r1d')
+DATABASE_NAME = environ.get('DATABASE_NAME', 'cfps')
+DATABASE_HOST = environ.get('DATABASE_HOST', '172.17.0.1')
+
+engine = create_engine(f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}")
+connection = engine.connect()
