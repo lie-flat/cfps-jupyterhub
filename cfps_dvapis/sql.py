c905e9d57c9e059157355b52b73fd1deceed0d56
kxxt <rsworktech@outlook.com>
2022-01-16 15:53:51 +0800

    Move sql related things to sql.py (cfps_dvapis)

--- cfps_dvapis/sql.py
+++ cfps_dvapis/sql.py
@@ -1,4 +1,6 @@
-from .sql import sql, connection, engine
+import pandas as pd
+from os import environ
+from sqlalchemy import create_engine
 
 DATABASE_PORT = environ.get('DATABASE_PORT', '3306')
 DATABASE_USER = environ.get('DATABASE_USER', 'jupyterhub')
@@ -8,3 +10,7 @@ DATABASE_HOST = environ.get('DATABASE_HOST', '172.17.0.1')
 
 engine = create_engine(f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}")
 connection = engine.connect()
+
+
+def sql(query, index_col=None, params=None):
+    return pd.read_sql(query, connection, index_col=index_col, params=params)
