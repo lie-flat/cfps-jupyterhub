e52961295c3ac17dc45af2ff7ff99cf306b9251f
kxxt <rsworktech@outlook.com>
2022-01-16 15:54:26 +0800

    Import pyecharts.

--- cfps_dvapis/__init__.py
+++ cfps_dvapis/__init__.py
@@ -1,10 +1,5 @@
 from .sql import sql, connection, engine
 
-DATABASE_PORT = environ.get('DATABASE_PORT', '3306')
-DATABASE_USER = environ.get('DATABASE_USER', 'jupyterhub')
-DATABASE_PASSWORD = environ.get('DATABASE_PASSWORD', 'SUPER-secret_PlavsW0r1d')
-DATABASE_NAME = environ.get('DATABASE_NAME', 'cfps')
-DATABASE_HOST = environ.get('DATABASE_HOST', '172.17.0.1')
-
-engine = create_engine(f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}")
-connection = engine.connect()
+from pyecharts import options as opts
+from pyecharts.charts import *
+from pyecharts.globals import *
