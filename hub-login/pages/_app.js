baf2ef43390ab2af3ddf1361502abda0df773573
kxxt <rsworktech@outlook.com>
2022-01-17 21:19:24 +0800

    fix: large icon while loading.

--- hub-login/pages/_app.js
+++ hub-login/pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
